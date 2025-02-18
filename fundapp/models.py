35cbd4741aca4b821ac6d564114f406bdbd63b4e
asyraphile <kaitolola@gmail.com>
2025-02-19 01:30:30 +0800

    created models and route

---
+++ fundapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import uuid
+# Create your models here.
+class Fund(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4, verbose_name="Fund ID")
+    name = models.CharField(max_length=255, verbose_name="Fund Name")
+    manager_name = models.CharField(max_length=255, verbose_name="Fund Manager Name")
+    description = models.TextField(default="", verbose_name="Fund Description")
+    nav = models.DecimalField(decimal_places=2, verbose_name="Fund Net Asset Value")
+    performance = models.DecimalField(decimal_places=2, verbose_name="Fund Performance(%)")
+    created_at = models.DateField(auto_now_add=True, editable=False, verbose_name="Fund Date of Creation")
+    
