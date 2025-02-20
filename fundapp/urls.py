280849ed3fc5d0456fb90cb773f10f2c747b37d0
asyraphile <kaitolola@gmail.com>
2025-02-21 01:51:42 +0800

    added router to urlpatterns

--- fundapp/urls.py
+++ fundapp/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-from .views import *
+from .views import FundViewset
 from rest_framework.routers import DefaultRouter
 #default urlpatterns for views or classview
 urlpatterns = [
-    path('fund/',)
+    path('fund/', FundViewset.as_view(), name="Fund")
     #implementing django rest framework
     #path('all/', views.all_funds, name="Retrieve all funds"),
     #path('all/', views.all_funds, name="Retrieve all funds"),
 ]
 router = DefaultRouter()
-router.register('fund',)
+router.register('fund', views.FundViewset, basename='fund')
+urlpatterns += router.urls
