d2439fb85a0a62af5496dfe3cc01e27edaf593d5
asyraphile <kaitolola@gmail.com>
2025-02-21 02:51:06 +0800

    commenting path in urlpatterns

--- fundapp/urls.py
+++ fundapp/urls.py
@@ -4,7 +4,7 @@ from .views import FundViewset
 from rest_framework.routers import DefaultRouter
 #default urlpatterns for views or classview
 urlpatterns = [
-    path('fund/', FundViewset.as_view(), name="Fund")
+    # path('fund/', FundViewset.as_view(), name="Fund")
     #implementing django rest framework
     #path('all/', views.all_funds, name="Retrieve all funds"),
     #path('all/', views.all_funds, name="Retrieve all funds"),
